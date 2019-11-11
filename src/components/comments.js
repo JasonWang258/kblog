a6638a51de74955c08e3a81ed6348797028a9530
Jason Wang <jasonwang31258@gmail.com>
2019-11-10 16:21:52 -0800

    commento

--- src/components/comments.js
+++ src/components/comments.js
@@ -4,8 +4,8 @@ import useScript from '@utils'
 
 export default function Comments () {
     let [loaded, error] = useScript('https://my.lestores.com:8888/js/commento.js', {
-        defer: true,
-        'data-auto-init': false
+        "defer": "true",
+        "data-auto-init": "false"
     })
     useEffect(() => {
         console.log('commento, useEffect')
