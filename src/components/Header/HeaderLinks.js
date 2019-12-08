1d6d09b1c4f2af7ba0d3920f966a52f5ca837972
Jason Wang <jasonwang31258@gmail.com>
2019-12-08 11:23:19 -0800

    new ui fix

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -30,7 +30,7 @@ export default function HeaderLinks(props) {
         <Tooltip
           id="test-tooltip"
           title="this is a test link"
-          placement={window.innerWidth > 959 ? "top" : "left"}
+          placement={window && window.innerWidth > 959 ? "top" : "left"}
           classes={{ tooltip: classes.tooltip }}
         >
           <Button
