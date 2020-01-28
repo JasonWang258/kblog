0ca2ed9518294185a5db4aceaf46e8fbe2e2e230
Jason Wang <jasonwang31258@gmail.com>
2020-01-27 19:28:57 -0800

    fix-inky

--- src/components/inky.js
+++ src/components/inky.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { useEffect } from 'react'
 import useScript from '@utils'
 import '@assets/css/style.css'
 
+const inkjs = require('@src/ink/ink.js')
 export default function Inky (props) {
     const { src } = props
-    window.inkjs = require('inkjs')
-    window.storyContent = require(`@src/ink/${src}.json`)
+    const storyContent = require(`@src/ink/${src}.json`)
+    useEffect(
+        () => {
+            // If there's no window there's nothing to do for us
+            if (! window) {
+                return
+            }
+            window.inkjs = inkjs
+            window.storyContent = storyContent
+        }
+    )
     useScript('/ink/main.js', JSON.stringify({
         defer: true
     }))
